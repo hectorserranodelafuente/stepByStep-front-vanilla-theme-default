156e9b55c4df445b2df8457050c84594960a92f5
hector <hserranodelafuente@gmail.com>
2025-05-05 21:53:19 +0200

    fix javascript email providers

--- theme/js/startIndex.js
+++ theme/js/startIndex.js
@@ -41,7 +41,7 @@
         imagesUrlEmailProviders.forEach( (email,index) => {
             
             row += `<div class="imgProvider">
-                        <img src="/public/img/logosProviders/aol.jpg">
+                        <img src="/public/img/logosProviders/${email}.jpg">
                     </div>`
             
             if(index%4===0||index===(imagesUrlEmailProviders.length-1)){
