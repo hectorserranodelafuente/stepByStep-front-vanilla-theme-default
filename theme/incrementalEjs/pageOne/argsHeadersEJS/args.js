aac5454d5d8e6d0c6bf9ae047f9af4e8355d7bce
hector <hserranodelafuente@gmail.com>
2025-06-19 17:20:31 +0200

    folder incrementalEjs

---
+++ theme/incrementalEjs/pageOne/argsHeadersEJS/args.js
@@ -0,0 +1,6 @@
+module.exports = {
+    scriptNames:['../../public/js/basicLoginForm'],
+    scriptNamesCordova:['../../js/basicLoginForm.js'],
+    scriptCss:['../../public/css/basicLoginForm.css'],
+    scriptCssCordova:['../../css/basicLoginForm.css']
+}
---
+++ theme/incrementalEjs/pageTwo/pageTwo_Two/nameFileDest.js
@@ -0,0 +1,1 @@
+exports.name = "pageTwo_Two.html"
