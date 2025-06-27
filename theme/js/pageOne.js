be5ab2355e28daead1e78a48f6b2cc0faa08afee
hector <hserranodelafuente@gmail.com>
2025-06-27 11:28:41 +0200

    incremental pageOne

--- theme/js/pageOne.js
+++ theme/js/pageOne.js
@@ -1,11 +1,27 @@
  async function getYogaList(){
-    const response = await fetch(`http://127.0.0.1:3000/incrementalApi/yogaDisciplines`, {
-            method:'GET',
-            headers: {'Content-Type':'application/json'},
-            body:JSON.stringify({tokenSession })
-    })
+    const response = await fetch(`http://127.0.0.1:3000/incrementalApi/yogaDisciplines`)
 
-    console.log(`Response ${response}`)
+    const data = await response.json()
+
+    let content = data.candidates[0].content.parts[0].text
+    
+    JSON.parse(content).forEach((yogaDiscipline)=>{
+        console.log(JSON.stringify(yogaDiscipline))
+        
+        let entity = `<div style="padding:20px;border:solid 1px;">
+                        <div style="background-color:#9bff99;">${yogaDiscipline.title}</div>
+                        <div >${yogaDiscipline.description}</div>
+                        </div>`
+        content += entity
+    
+        })
+                                                                        
+        
+        console.log(content)
+    
+    document.getElementById("yogaDisciplinesList").innerHTML= content
+
+    //console.log(`Response ${response}`)
  }
 
- getYogaList()
+getYogaList()
