42571b22c05d5c2d4028acd2f33e9d496d640360
hector <hserranodelafuente@gmail.com>
2025-06-27 05:52:58 +0200

    reducing number of incrementalEjs views

--- theme/incrementalEjs/pageTwo/pageTwo_One/argsHeadersEJS/args.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    scriptNames:['../../public/js/basicLoginForm'],
-    scriptNamesCordova:['../../js/basicLoginForm.js'],
-    scriptCss:['../../public/css/basicLoginForm.css'],
-    scriptCssCordova:['../../css/basicLoginForm.css']
-}
