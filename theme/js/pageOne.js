8bf999e4840c5db725ba2e844d468244a3c57a8d
hector <hserranodelafuente@gmail.com>
2025-06-27 09:44:20 +0200

    pageOne.js

--- theme/js/pageOne.js
+++ theme/js/pageOne.js
@@ -1,7 +1,11 @@
- const response = await fetch(`http://127.0.0.1:3000/incrementalApi/yogaDisciplines`, {
+ async function getYogaList(){
+    const response = await fetch(`http://127.0.0.1:3000/incrementalApi/yogaDisciplines`, {
             method:'GET',
             headers: {'Content-Type':'application/json'},
             body:JSON.stringify({tokenSession })
-})
+    })
 
-console.log(`Response ${response}`)
+    console.log(`Response ${response}`)
+ }
+
+ getYogaList()
