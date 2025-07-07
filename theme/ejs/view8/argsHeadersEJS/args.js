24e3641bdb1a99f6e4a605a8e3f2964eecb80583
hector <hserranodelafuente@gmail.com>
2025-07-07 19:36:57 +0200

    variable on scriptName

--- theme/ejs/view8/argsHeadersEJS/args.js
+++ theme/ejs/view8/argsHeadersEJS/args.js
@@ -1,3 +1,8 @@
+
+const env = require('../env.js')
 module.exports = {
-    scriptNames:['../../public/js/redirectionLoading.js','../../public/js/cubical-expansion.js']
+    scriptNames:[
+        '../../public/js/redirectionLoading.js',
+        `../..${env.development.pathJsFile}`
+    ]
 }
